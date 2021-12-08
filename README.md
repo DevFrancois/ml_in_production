a61fa6cecb10e6519a5c70e4b5ed29b2752b41bd
Francois <dev@francois>
2021-12-08 13:42:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+100. First list item
+     - First nested list item
+       - Second nested list item
+
+![Jedha logo](https://uploads-ssl.webflow.com/5eaab2907ba1269e03897c89/5ed80037f18ada5255d3b0bd_logo-jedha-footer.svg)
---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, jsonify
+import joblib
+import numpy as np
+
+model = joblib.load("models/model.joblib")
+
+
+app = Flask(__name__)
+
+
+@app.route("/documentation")
+def more_love():
+    things_i_love = [
+        "Star Wars",
+        "Coffee",
+        "Cookies",
+    ]
+    return render_template("documentation.html", things_i_love=things_i_love)
+
+
+# @app.route("/documentation")
+# def prediction():
+#     things_i_love = [
+#         "Star Wars",
+#         "Coffee",
+#         "Cookies",
+#     ]
+#     return render_template("prediction.html", things_i_love=things_i_love)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # Check if request has a JSON content
+    if request.json:
+        # Get the JSON as dictionnary
+        data = request.get_json()
+        # Check mandatory key
+        if "input" in data.keys():
+            # Load model
+            # Predict
+            input = np.array(data['input'])
+            # print(input)
+            # prediction = model.predict([data["input"]])
+
+            prediction = model.predict(input)
+
+            # Return the result as JSON but first we need to transform the
+            # result so as to be serializable by jsonify()
+            # prediction = str(prediction[0])
+
+            # result = {'result': prediction.tolist()}
+            result = {'prediction': prediction.tolist()}
+
+            # return jsonify({"predict": prediction}), 200
+
+            return jsonify(result), 200
+            # return input
+    return jsonify({"msg": "Error: not a JSON or no email key in your request"})
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/doc")
+def doc():
+    return render_template("doc.html")
+
+
+@app.route("/test")
+def test():
+    return render_template("title.html")
+
+
+@app.route("/coucou")
+def coucou():
+    return render_template("coucou.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
